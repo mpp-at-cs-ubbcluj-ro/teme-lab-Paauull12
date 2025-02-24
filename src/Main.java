3f35551c87aeb36f4d71eb552a79ce2434b5d5b2
Paauull12 <andrei.dobrescu@stud.ubbcluj.ro>
2025-02-24 17:49:07 +0200

    first commit for mpp

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        // Display a message on the console
+        System.out.println("Hello, World!");
+
+        // Display some additional information
+        System.out.println("This is a simple Java program");
+        System.out.println("Current time in milliseconds: " + System.currentTimeMillis());
+
+        // Display a formatted message
+        String name = "Java User";
+        int year = 2025;
+        System.out.printf("Welcome, %s! It's now %d.\n", name, year);
+    }
+}
