df5f7a3f8ec4fbdde8b08227fc5d6719fea241f7
Paauull12 <andrei.dobrescu@stud.ubbcluj.ro>
2025-03-11 00:03:50 +0200

    feat!: added update test in MainDB

--- gradlewithdb/src/main/java/ro/mpp2025/MainBD.java
+++ gradlewithdb/src/main/java/ro/mpp2025/MainBD.java
@@ -2,7 +2,9 @@ package ro.mpp2025;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Properties;
+import java.util.stream.StreamSupport;
 
 public class MainBD {
     public static void main(String[] args) {
@@ -19,7 +21,28 @@ public class MainBD {
         System.out.println("Toate masinile din db");
         for(Car car:carRepo.findAll())
             System.out.println(car);
-       String manufacturer="Tesla";
+
+
+        System.out.println("Updating car with ID 2 from the database");
+
+        Optional<Car> optionalCar = StreamSupport.stream(carRepo.findAll().spliterator(), false)
+                .filter(car -> car.getId() == 2)
+                .findFirst();
+
+        if (optionalCar.isPresent()) {
+            Car car = optionalCar.get();
+
+            car.setModel("Updated Model");
+            car.setYear(2023);
+
+            carRepo.update(car.getId(), car);
+
+            System.out.println("Car updated successfully: " + car);
+        } else {
+            System.out.println("Car with ID 2 not found.");
+        }
+
+        String manufacturer="Tesla";
         System.out.println("Masinile produse de "+manufacturer);
         for(Car car:carRepo.findByManufacturer(manufacturer))
             System.out.println(car);
